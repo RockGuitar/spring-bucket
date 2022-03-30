97e701e0ea5a06291a9ee62c59fdb6c7794464d1
Никита <oleinik.nikita96@gmail.com>
2022-03-30 20:45:10 +0300

    Initial commit

---
+++ src/main/java/springbucket/SpringBucketApplication.java
@@ -0,0 +1,13 @@
+package springbucket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBucketApplication {
+
+    public static void main ( String[] args ) {
+        SpringApplication.run(SpringBucketApplication.class, args);
+    }
+
+}
